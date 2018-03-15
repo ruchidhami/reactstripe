d82a4a83dd8e24f12a960ff0c9c9de794fd5a9cd
shreeramneupane <shreeramneupane0@gmail.com>
2018-03-15 17:08:55 +0545

    Setup material ui library

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton'
 import logo from './logo.svg';
 import './App.css';
+
+
+// Theme
+import { deepOrange500 } from 'material-ui/styles/colors'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+// Theme
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500
+  }
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <form>
+            <RaisedButton type='submit' label='Submit' primary />
+            </form>
+          </p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
