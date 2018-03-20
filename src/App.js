c02b1240fd9d2084046f733de06f920d396d5aee
shreeramneupane <shreeramneupane0@gmail.com>
2018-03-20 08:04:59 +0545

    Setup Darktheme

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react';
-import RaisedButton from 'material-ui/RaisedButton'
-import logo from './logo.svg';
-import './App.css';
 
+// Material Component
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+// Font
+import 'typeface-roboto'
 
 // Theme
-import { deepOrange500 } from 'material-ui/styles/colors'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
-// Theme
-const muiTheme = getMuiTheme({
-  palette: {
-    accent1Color: deepOrange500
-  }
-})
+const muiTheme = getMuiTheme(darkBaseTheme);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleDrawerOpen = () => this.setState({open: true});
+  handleDrawerClose = () => this.setState({open: false});
+
   render() {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-            <form>
-            <RaisedButton type='submit' label='Submit' primary />
-            </form>
-          </p>
+        <div>
+          <AppBar title="React Stripe App" onLeftIconButtonClick = { this.handleDrawerOpen } />
+
+          <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={ this.handleDrawerClose }>
+            <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+            <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+          </Drawer>
         </div>
       </MuiThemeProvider>
     );
